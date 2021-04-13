939188c852eb0b8784629ab50573451169e98049
Arman <91827364550ara@gmail.com>
2021-04-13 22:25:10 +0300

    commit_1

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,15 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    param = request.GET.get('from-landing', '')
+    if param == 'original':
+        counter_click.update([param])
+        # print("Клики", counter_click.most_common())
+        return render(request, 'index.html')
+    elif param == 'test':
+        counter_click.update([param])
+        # print("Клики", counter_click.most_common())
+        return render(request, 'index.html')
     return render(request, 'index.html')
 
 
@@ -20,13 +29,31 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    param = request.GET.get('ab-test-arg', 'original')
+    if param == 'original':
+        counter_show.update([param])
+        # print("Показы", counter_show.most_common())
+        return render(request, 'landing.html')
+    elif param == 'test':
+        counter_show.update([param])
+        # print("Показы", counter_show.most_common())
+        return render(request, 'landing_alternate.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
-    return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
-    })
+    try:
+        original_stats = counter_click['original']/counter_show['original']
+        test_stats = counter_click['test']/counter_show['test']
+        return render(request, 'stats.html', context={
+            'original_conversion': original_stats,
+            'test_conversion': test_stats,
+        })
+    except ZeroDivisionError:
+        return render(request, 'stats.html', context={
+            'original_conversion': f"Показы: {counter_show['original']} "
+                                   f"Клики: {counter_click['original']}",
+            'test_conversion': f"Показы: {counter_show['test']} "
+                               f"Клики: {counter_click['test']}",
+        })
